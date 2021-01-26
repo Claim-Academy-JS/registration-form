30eb1503e6410fc3cfbb70faf96b0167b0746720
Manav Misra <manavm@visionwebsoft.com>
2021-01-26 15:03:33 -0600

    :construction: Select submit button

--- scripts.js
+++ scripts.js
@@ -6,7 +6,10 @@
 /**
  * When the toggleBtn is clicked, hide all the inputs except for username and password
  */
+const submitBtn = document.querySelector('button[type="submit"]');
 const toggleBtn = document.querySelector("#toggle-btn");
+
+console.log(submitBtn);
 
 toggleBtn.addEventListener("click", function () {
   this.textContent = "Need to create an account?";
