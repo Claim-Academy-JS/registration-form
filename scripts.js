ced18db14ba8666eba4d9626025fd15f1598174a
Manav Misra <manavm@visionwebsoft.com>
2021-01-26 14:44:18 -0600

    Grab a reference the other button that says 'already.'

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
 /**
- * TODO: username must be greater than 8 characters
- * If I leave the field and it is less than 8 characters, mark it with an error
+ * TODO: When we click 'Already,' then all fields should be hidden EXCEPT
+ * 'username' and 'password.'
  */
-document.querySelector("#uname").addEventListener(
-  "input",
 
-  // Wow! We can pass a function into another fxn./method - this is known as callback fxn.
-  () => {
-    console.log("hello! I was changed!");
-  }
-);
+/**
+ * 1.
+ */
+const toggleBtn = document.querySelector("#toggle-btn");
+console.log(toggleBtn);
