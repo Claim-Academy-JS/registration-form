5531a7295172a6ad79d1daf13dec1e2934b74d3d
Manav Misra <manavm@visionwebsoft.com>
2021-01-26 15:35:16 -0600

    :poop: Toggle hidden fields based on clickin

--- scripts.js
+++ scripts.js
@@ -1,18 +1,23 @@
-/**
- * When the toggleBtn is clicked, hide all the inputs except for username and password
- */
 const submitBtn = document.querySelector('button[type="submit"]');
 const toggleBtn = document.querySelector("#toggle-btn");
 
 const toggledInputs = document.querySelectorAll("input:not(#uname, #password)");
 
-console.log(toggledInputs, "hi");
+// TODO: Iterate over all toggledInputs and add the 'is-hidden' class
 
 toggleBtn.addEventListener("click", function () {
   if (this.innerText === "Already have an account?") {
+    for (let i = 0; i < toggledInputs.length; i += 1) {
+      toggledInputs[i].classList.add("is-hidden");
+    }
     this.innerText = "Need to create an account?";
     submitBtn.innerText = "Login!";
   } else {
+    const hiddenInputs = document.querySelectorAll("input.is-hidden");
+    for (let i = 0; i < hiddenInputs.length; i += 1) {
+      hiddenInputs[i].classList.add("is-visible");
+    }
+
     this.innerText = "Already have an account?";
     submitBtn.innerText = "Register!";
   }
