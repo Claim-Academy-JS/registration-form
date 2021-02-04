a28379863e9fcc50262b00d1dfbe09136a257115
Manav Misra <manavm@visionwebsoft.com>
2021-02-04 17:53:53 -0600

    :construction: Add validation for blank inputs

--- scripts.js
+++ scripts.js
@@ -4,12 +4,26 @@ const needMsg = "Need to create an account?";
 const submitBtn = document.querySelector('button[type="submit"]');
 const toggleBtn = document.querySelector("#toggle-btn");
 
+const inputs = document.querySelectorAll("input");
+
 const toggledInputDivs = Array.from(document.querySelectorAll("div")).filter(
   (div) => {
     const input = div.querySelector("input");
 
     return input && input.id !== "uname" && input.id !== "password";
   }
+);
+
+inputs.forEach((input) =>
+  input.addEventListener("blur", function handleBlur() {
+    if (this.value.length) {
+      this.classList.replace("is-error", "is-valid") ||
+        this.classList.add("is-valid");
+    } else {
+      this.classList.replace("is-valid", "is-error") ||
+        this.classList.add("is-error");
+    }
+  })
 );
 
 function toggleFormInfo() {
